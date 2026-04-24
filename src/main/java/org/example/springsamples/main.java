227eb527c7c1eb872f3b2e774074447bf201113c
mahesh <maheshmadala757@gmail.com>
2026-04-24 11:28:36 +0530

    Update Changes

--- src/main/java/org/example/springsamples/main.java
+++ src/main/java/org/example/springsamples/main.java
@@ -7,58 +7,93 @@ import org.example.springsamples.service.AccountService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfigDatajpa.class);
         AccountService accountService = ctx.getBean(AccountService.class);
-         Account acc1 = new Account();
-        acc1.setAccountHolderName("mahesh");
-        acc1.setAccountNumber("23456");
-        acc1.setBalance(5000);
-        acc1.setAccountType("Savings");
-        acc1.setEmail("mahesh@gmail.com");
-        acc1.setPhone("78906543");
-        acc1.setCity("Nellore");
-        accountService.createAccount(acc1);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
 
-        Account acc2 = new Account();
-        acc2.setAccountHolderName("mahesh");
-        acc2.setAccountNumber("23456");
-        acc2.setBalance(5000);
-        acc2.setAccountType("Savings");
-        acc2.setEmail("mahesh@gmail.com");
-        acc2.setPhone("78906543");
-        acc2.setCity("Nellore");
-        accountService.createAccount(acc2);
+            System.out.println("\n1. Create Account");
+            System.out.println("2. View All Accounts");
+            System.out.println("3. Deposit");
+            System.out.println("4. Withdraw");
+            System.out.println("5. Transfer");
+            System.out.println("6. Exit");
+            System.out.print("Enter Choice: ");
 
-        List<Account> accounts = accountService.getAccount();
+            int choice = sc.nextInt();
+            sc.nextLine();
 
-        Long acc1Id = accounts.get(0).getAccId();
-        Long acc2Id = accounts.get(1).getAccId();
+            switch (choice) {
 
-        accountService.deposit(acc1Id,1000);
+                case 1:
 
-        accountService.withdraw(acc1Id,100);
+                    Account acc1 = new Account();
 
-        accountService.transfer(acc1Id, acc2Id,500);
+                    System.out.println("Name:");
+                    acc1.setAccountHolderName(sc.nextLine());
 
-        System.out.println("Transactions of Account1");
+                    System.out.println("Account Number:");
+                    acc1.setAccountNumber(sc.nextLine());
 
-        accountService.printTransactions(acc1Id);
+                    System.out.println("Balance");
+                    acc1.setBalance(sc.nextDouble());
 
-        System.out.println("Transactions of Account2");
+                    System.out.println("Account Type:");
+                    acc1.setAccountType(sc.nextLine());
 
-        accountService.printTransactions(acc2Id);
+                    System.out.println("Email:");
+                    acc1.setEmail(sc.nextLine());
 
-        System.out.println(accounts);
+                    System.out.println("Phone:");
+                    acc1.setPhone(sc.nextLine());
 
+                    System.out.println("City:");
+                    acc1.setCity(sc.nextLine());
 
+                    accountService.createAccount(acc1);
+                    System.out.println("Account Created:");
+                    break;
+                case 2:
+                    List<Account> accounts = accountService.getAccount();
+                    for (Account a : accounts) {
+                        System.out.println("ID: " + a.getAccId() +
+                                ", Name: " + a.getAccountHolderName() +
+                                ", Balance: " + a.getBalance());
+                    }
+                    break;
+                case 3:
+                    System.out.println(" Enter Account ID :");
+                    Long dId = sc.nextLong();
+                    System.out.println("Enter Amount:");
+                    double dAmt = sc.nextDouble();
+                    accountService.deposit(dId, dAmt);
+                    break;
+                case 4:
+                    System.out.println("Enter Account ID:");
+                    Long wId = sc.nextLong();
+                    System.out.println("Enter Amount:");
+                    double wAmt = sc.nextDouble();
+                    accountService.withdraw(wId, wAmt);
 
+                    break;
+                case 5:
+                    System.out.println("From Account ID:");
+                    Long fromId = sc.nextLong();
+                    System.out.println("To Account ID:");
+                    Long toId = sc.nextLong();
+                    accountService.deposit(fromId, toId);
+                    break;
+                case 6:
+                    System.out.println("Exit ");
+                    System.exit(0);
 
-       // accountService.deletewithFailure(45L);
-
-        System.out.println(accountService.getAccount());
-
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
     }
 }
